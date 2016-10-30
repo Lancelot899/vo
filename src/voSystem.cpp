dcb074b018717c21cbfdd44db98bbb835d5b9dfe
lancelot <3128243880@qq.com>
2016-10-30 15:28:06 +0800

    vo system build start

--- src/voSystem.cpp
+++ src/voSystem.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "voSystem.h"
 
 #include "ImgIO.h"
@@ -5,4 +7,23 @@
 
 voSystem::voSystem()
 {
+    imgIO = std::make_shared<ImgIO>();
+}
+
+bool voSystem::running()
+{
+    trackThread = new boost::thread(std::bind(&voSystem::tracking, this));
+    optThread = new boost::thread(std::bind(&voSystem::optimize, this));
+
+    return true;
+}
+
+void voSystem::tracking()
+{
+    printf("tracking!\n");
+}
+
+void voSystem::optimize()
+{
+    printf("optimize!\n");
 }
